5d6351eaef07f481037319e58efb820bdb518c1d
jimyungkoh <kojimyung1206@naver.com>
2022-01-11 22:14:48 +0900

    다중 통화를 지원하는 Money 객체-빨강 코드 작성

---
+++ section1/moneyExample/src/moneyExample.java
@@ -0,0 +1,13 @@
+// test-driven-development/section1/moneyExample/src/moneyExample.java
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class moneyExample {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10,five.amount);
+    }
+}
