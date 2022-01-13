7f04867352c3c0bd9b60eb265001ca1f679e2e25
jimyungkoh <kojimyung1206@naver.com>
2022-01-13 18:33:40 +0900

    [추가] equalityForAll 챕터  화폐 에제 기존 코드

--- equalityForAll/src/moneyExample.java
+++ equalityForAll/src/moneyExample.java
@@ -1,9 +1,22 @@
 // test-driven-development/section1/equalityForAll/src/Dollar.java
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class moneyExample {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
 
+        // 계산된 금액을 저장하는 totalPrice 객체 생성
+        Dollar totalPrice = five.times(2);
+        assertEquals(10, totalPrice.amount);
+
+        // 5달러짜리 주식 세개의 금액은?
+        totalPrice = five.times(3);
+        assertEquals(15,totalPrice.amount);
+
+        //times(int) 연산을 계속 했음에도 five.amount는 그대로 5인가?
+        assertEquals(5, five.amount);
+    }
 }
