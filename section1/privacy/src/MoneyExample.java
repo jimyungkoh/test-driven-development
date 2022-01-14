60587e2dd9b29675f46f91419b7de1c57e8fb105
jimyungkoh <kojimyung1206@naver.com>
2022-01-14 21:27:44 +0900

    [수정] 외부에서 사용할 일 없는 amount 변수 private 변수로 변경

--- section1/privacy/src/MoneyExample.java
+++ section1/privacy/src/MoneyExample.java
@@ -9,15 +9,13 @@ public class MoneyExample {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
 
-        // 계산된 금액을 저장하는 totalPrice 객체 생성
-        Dollar totalPrice = five.times(2);
-        assertEquals(new Dollar(2), totalPrice);
+        // 5달러 주식 2주는 10달러와 같은가?
+        assertEquals(new Dollar(10), five.times(2));
 
-        // 5달러짜리 주식 세개의 금액은?
-        totalPrice = five.times(3);
-        assertEquals(new Dollar(3), totalPrice);
+        // 5달러 주식 3주는 15달러와 같은가?
+        assertEquals(new Dollar(15), five.times(3));
 
-        //times(int) 연산을 계속 했음에도 five.amount는 그대로 5인가?
+        //times(int) 연산을 계속 했음에도 five는 그대로 5달거인가?
         assertEquals(new Dollar(5), five);
     }
 
