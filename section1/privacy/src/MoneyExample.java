4aaec5ac38b617f29c5109f3f903206a594b2ed7
jimyungkoh <kojimyung1206@naver.com>
2022-01-14 21:15:56 +0900

    [수정] 정수 간 비교 테스트 -> 객체 간 비교 테스트

--- section1/privacy/src/MoneyExample.java
+++ section1/privacy/src/MoneyExample.java
@@ -11,14 +11,14 @@ public class MoneyExample {
 
         // 계산된 금액을 저장하는 totalPrice 객체 생성
         Dollar totalPrice = five.times(2);
-        assertEquals(10, totalPrice.amount);
+        assertEquals(new Dollar(2), totalPrice);
 
         // 5달러짜리 주식 세개의 금액은?
         totalPrice = five.times(3);
-        assertEquals(15, totalPrice.amount);
+        assertEquals(new Dollar(3), totalPrice);
 
         //times(int) 연산을 계속 했음에도 five.amount는 그대로 5인가?
-        assertEquals(5, five.amount);
+        assertEquals(new Dollar(5), five);
     }
 
     //값 객체의 동일성을 확인하는 'testEquality()' 메서드
