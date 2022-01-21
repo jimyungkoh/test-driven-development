2bb12c5b07d16172b13bffbb3f827848f279880a
jimyungkoh <kojimyung1206@naver.com>
2022-01-21 16:47:35 +0900

    [수정] 반환 타입을 Money로 변경

---
+++ section1/makingObjects/src/Money.java
@@ -0,0 +1,42 @@
+//section1/applesAndOranges/src/Money.java
+
+public class Money {
+    protected int amount;
+
+    //동일성 일반화
+    public boolean equals(Object object){
+        Money money = (Money) object;
+        //new Dollar(5)의 amount가 equals()
+        //  메서드의 인자인 new Dollar(6)의 amount와 같은지 확인한다.
+        return amount == money.amount
+                // 현재 클래스가 진짜 현재 객체의 클래스와 같아?
+                && getClass().equals(money.getClass());
+    }
+}
+
+class Dollar extends Money{
+    Dollar(int amount){
+        this.amount = amount;
+    }
+
+    // 반환 타입을 슈퍼 클래스로 설정함
+    Money times(int multiplier) {
+        // amount 값을 amount*multiplier로 설정한 새로운 Dollar 객체 생성
+        return new Dollar(amount*multiplier);
+    }
+
+
+}
+
+class Won extends Money{
+    Won(int amount){
+        this.amount = amount;
+    }
+
+    // 반환 타입을 슈퍼 클래스로 설정함
+    Money times(int multiplier) {
+        // amount 값을 amount*multiplier로 설정한 새로운 Won 객체 생성
+        return new Won(amount*multiplier);
+    }
+}
+
