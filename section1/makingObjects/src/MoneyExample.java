32180bb80461f509e06b5a6be5728eb47eeeb38c
jimyungkoh <kojimyung1206@naver.com>
2022-01-21 17:17:48 +0900

    [수정] 팩토리 메서드를 사용하도록 테스트 코드 수정

--- section1/makingObjects/src/MoneyExample.java
+++ section1/makingObjects/src/MoneyExample.java
@@ -11,13 +11,13 @@ public class MoneyExample {
         Money five = Money.dollar(5);
 
         // 5달러 주식 2주는 10달러와 같은가?
-        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(Money.dollar(10), five.times(2));
 
         // 5달러 주식 3주는 15달러와 같은가?
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(15), five.times(3));
 
         //times(int) 연산을 계속 했음에도 five는 그대로 5달거인가?
-        assertEquals(new Dollar(5), five);
+        assertEquals(Money.dollar(5), five);
     }
 
     @Test
@@ -26,34 +26,33 @@ public class MoneyExample {
         Money fiveK = Money.won(5000);
 
         // 5000원짜리 주식 2주는 10000원과 같은가?
-        assertEquals(new Won(10000), fiveK.times(2));
+        assertEquals(Money.won(10000), fiveK.times(2));
 
         // 5000원짜리 주식 3주는 15000원과 같은가?
-        assertEquals(new Won(15000), fiveK.times(3));
+        assertEquals(Money.won(15000), fiveK.times(3));
 
         //times(int) 연산을 계속 했음에도 fiveK는 그대로 5000원인가?
-        assertEquals(new Won(5000), fiveK);
+        assertEquals(Money.won(5000), fiveK);
     }
 
     //값 객체의 동일성을 확인하는 'testEquality()' 메서드
     @Test
     public void testEquality() {
         //같은 값을 가진 달러 객체가 정말 같은지 확인하는 테스트 케이스
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        //다른 값을 가진 달러 객체가 정말 다른지 확인하는 테스트 케이스
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-        //같은 값을 가진 원 객체가 정말 같은지 확인하는 테스트 케이스
-        assertTrue(new Won(5000).equals(new Won(5000)));
-        //다른 값을 가진 원 객체가 정말 다른지 확인하는 테스트 케이스
-        assertFalse(new Won(5000).equals(new Won(6000)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 
-//        Won과 Dollar의 객체가 정말 다른지 확인하는 테스트 케이스
-//          -> 정상이라면  500원은 500달러와 달라야 한다(assertFalse 테스트가 성공해야 한다!)
-        assertFalse(new Won(500).equals(new Dollar(500)));
+        //다른 값을 가진 달러 객체가 정말 다른지 확인하는 테스트 케이스
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+
+        //같은 값을 가진 원 객체가 정말 같은지 확인하는 테스트 케이스
+        assertTrue(Money.won(5000).equals(Money.won(5000)));
+
+        //다른 값을 가진 원 객체가 정말 다른지 확인하는 테스트 케이스
+        assertFalse(Money.won(5000).equals(Money.won(6000)));
     }
 
     @Test
     public void testInequality(){
-        assertFalse(new Won(500).equals(new Dollar(500)));
+        assertFalse(Money.won(500).equals(Money.dollar(500)));
     }
 }
